506602861964c39de3ebbdefe79a369a26631b8a
EitanBal <gabrieldok03@gmail.com>
2025-07-01 13:47:58 +0200

    Delete Product listing system directory

--- Product listing system/Product_Portal/admin/approve.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../includes/db.php';
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $id = $_POST['id'];
-    $conn->query("UPDATE products SET status = 'approved' WHERE id = $id");
-    header("Location: index.php");
-}
-?>
